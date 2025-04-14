6ed03b6edfb47903d9da2483a1f8c73e237acdb4
Raunak <192615753+RaunakBohra@users.noreply.github.com>
2025-04-14 12:44:28 +0545

    Added buttons that work inplace of prompts for user inputs

--- javascript.js
+++ javascript.js
@@ -26,14 +26,61 @@ function getComputerChoice() {
 
 }
 
+const container = document.querySelector("#container");
 
-function getHumanChoice() {
-    var hc = prompt("Enter Rock,Paper or Scissor");
-    hc = hc.toLowerCase();
+
+const content = document.createElement("p");
+content.textContent = " Welcome to RPS, click button below to start"
+
+content.innerHTML = ` <button class= "btn" id="rock">Rock</button>    
+    <button class= "btn" id="paper">Paper</button>
+    <button class= "btn"  id="scissor">Scissor</button>`;
+    const btn = document.querySelector(".btn");
+
+
+container.appendChild(content);
+
+
+const rock = document.querySelector("#rock");
+rock.addEventListener("click", ()=>{
+    hc="rock";
+    const computerSelection = getComputerChoice();
+    playRound(hc,computerSelection)
+    ;
+
+})
+
+const paper = document.querySelector("#paper");
+paper.addEventListener("click", ()=>{
+    hc="paper";
     console.log(hc);
-    return hc;
+    const computerSelection = getComputerChoice();
+    playRound(hc,computerSelection)
+    ;
 
-}
+})
+const scissor = document.querySelector("#scissor")
+scissor.addEventListener("click", ()=>{
+    hc="scissor";
+    console.log(hc);
+    const computerSelection = getComputerChoice();
+    playRound(hc,computerSelection)
+    ;
+
+})
+
+
+
+ //function getHumanChoice() {
+
+
+//    var hc = prompt("Enter Rock,Paper or Scissor");
+
+//      hc = hc.toLowerCase();
+// console.log(hc);
+//   return hc;
+
+ // }
 
 
 function playRound(hc, computerchoice) {
@@ -86,18 +133,18 @@ function playRound(hc, computerchoice) {
 
 
 
-for (let i = 1; i <= 5; i++) {
+// for (let i = 1; i <= 1; i++) {
 
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
+//     const humanSelection = getHumanChoice();
+//     const computerSelection = getComputerChoice();
 
-    playRound(humanSelection, computerSelection);
-    console.log(`Round ${i}`);
-}
+//     playRound(humanSelection, computerSelection);
+//     console.log(`Round ${i}`);
+// }
 
-if (humanScore > computerScore) {
-    console.log(`User score: ${humanScore} \nComputer Score: ${computerScore} \nYou are the winner`)
-}
-else {
-    console.log(`User score: ${humanScore} \nComputer Score: ${computerScore} \nYou are the looser`)
-}
+// if (humanScore > computerScore) {
+//     console.log(`User score: ${humanScore} \nComputer Score: ${computerScore} \nYou are the winner`)
+// }
+// else {
+//     console.log(`User score: ${humanScore} \nComputer Score: ${computerScore} \nYou are the looser`)
+// }
